ba26a5babb5f272377d16906def9dacae3eb954f
missumissumissme <43581033+missumissumissme@users.noreply.github.com>
2019-02-18 10:41:03 -0500

    Add files via upload

---
+++ deep space 2/src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
+import frc.robot.commands.Drive;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class Elevator extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  private VictorSP m_Up;
+  private VictorSP m_Down;
+  private DifferentialDrive m_;
+
+
+  public DriveTrain(){
+    m_Left = new VictorSP(RobotMap.Leftdrive);
+    m_Right = new VictorSP(RobotMap.Rightdrive);
+    m_Drive = new DifferentialDrive(m_Left, m_Right);
+}
+
+public void drive ( double moveValue, double rotateValue){
+
+  m_Drive.arcadeDrive (moveValue, rotateValue);
+}
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
